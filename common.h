a0ffc351c205324bc82042065c3448fdb8f93d16
Gray Lin <gray.lin@primax.com.tw>
2024-09-30 16:01:28 +0800

    u

--- common.h
+++ common.h
@@ -11,21 +11,22 @@
 #define __COMMON_H__
 
 /* Constants defined by configure.ac */
-#define HAVE_INTTYPES_H 1
-#define HAVE_STDINT_H 1
+// #define HAVE_INTTYPES_H 1
+// #define HAVE_STDINT_H 1
 
-#ifdef HAVE_INTTYPES_H
-#include <inttypes.h>
-#endif
-#ifdef HAVE_STDINT_H
-# ifndef _MSC_VER
-# include <stdint.h>
-# else
-# include "stdint.h"
-# endif
-#endif
-#include <stdbool.h>
-#include <string.h>
+// #ifdef HAVE_INTTYPES_H
+// #include <inttypes.h>
+// #endif
+// #ifdef HAVE_STDINT_H
+// # ifndef _MSC_VER
+// # include <stdint.h>
+// # else
+// # include "stdint.h"
+// # endif
+// #endif
+// #include <stdbool.h>
+
+#include <string>
 #include <ctime>
 
 /* define the tty color */
