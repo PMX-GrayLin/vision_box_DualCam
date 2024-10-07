0e07c36e49c68966ecf8154358b443e2eee116b4
Gray Lin <gray.lin@primax.com.tw>
2024-10-07 20:13:32 +0800

    z

--- ext_mqtt_client.hpp
+++ ext_mqtt_client.hpp
@@ -5,19 +5,19 @@
 #ifndef __EXT_MQTT_CLIENT_H__
 #define __EXT_MQTT_CLIENT_H__
 
-int32_t ext_mqtt_subscriber_Dual();
-int32_t ext_mqtt_publisher_Dual(char *jString, const bool bCameID);  // dual camera
-void ext_mqtt_release();
+// int32_t ext_mqtt_subscriber_Dual();
+// int32_t ext_mqtt_publisher_Dual(char *jString, const bool bCameID);  // dual camera
+// void ext_mqtt_release();
 
 /* EXPORTED SUBPROGRAM SPECIFICATION ------------------------------------------------- */
 #ifdef __cplusplus
 extern "C"
 {
 #endif
-    // extern int32_t ext_mqtt_subscriber_Dual();
-    // extern int32_t ext_mqtt_publisher_Dual(char *jString, const bool bCameID); //dual camera
+    extern int32_t ext_mqtt_subscriber_Dual();
+    extern int32_t ext_mqtt_publisher_Dual(char *jString, const bool bCameID); //dual camera
 
-    // extern void ext_mqtt_release();
+    extern void ext_mqtt_release();
 
 #ifdef __cplusplus
 }
