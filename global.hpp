bbbe1d63104152f0d989de87d1e49e4663c1b79f
Gray Lin <gray.lin@primax.com.tw>
2024-10-09 17:14:16 +0800

    z

--- global.hpp
+++ global.hpp
@@ -2,10 +2,24 @@
 
 #include <cstdint>
 
+#ifdef __cplusplus
+
 #define DEBUGX
+
+void xlog(const std::string& message) {
+    #ifdef DEBUGX
+    // Log to console
+    std::cout << message << std::endl;
+    #endif
+}
+#elif
+
 #ifndef DEBUGX
 #define xlog(...) ((void)0)
 #else
 #define xlog(...) printf(__VA_ARGS__)
 #endif
+
+#endif
+
 
